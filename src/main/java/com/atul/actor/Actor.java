163502299158dd290410da3260870f30454a0333
Atul Soman <atul.soman@microfocus.com>
2019-05-30 12:08:24 +0530

    nitial commit

---
+++ src/main/java/com/atul/actor/Actor.java
@@ -0,0 +1,10 @@
+package com.atul.actor;
+
+/**
+ * The ator interface which clients have to consume
+ * @author SAtul
+ *
+ */
+public interface Actor {
+	public void sendMessage(String payload);
+}
